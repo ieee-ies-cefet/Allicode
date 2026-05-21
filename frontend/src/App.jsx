f1b6fb4a3ce0f85c01a18ae18fe3d8cd280b58e0
FelipeDobao <dobaofelipe@gmail.com>
2026-05-21 16:51:37 -0300

    home - criando a pagina principal

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,121 +1,93 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
+import logo from './assets/Logo.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <section id="center">
-        <div className="hero">
-          <img src={heroImg} className="base" width="170" height="179" alt="" />
-          <img src={reactLogo} className="framework" alt="React logo" />
-          <img src={viteLogo} className="vite" alt="Vite logo" />
-        </div>
-        <div>
-          <h1>Get started</h1>
+      <header>
+          <img src={logo} alt='Logo IEES IES' />
+          <div  className='navegacao'>
+            <nav>
+            <ul>
+              <li><a href="">SOBRE</a></li>
+              <li><a href=""> CONTATO</a></li>
+              <li><a href="">CONTA</a></li>
+              <li><a href="">PROJETOS</a></li>
+            </ul>
+          </nav>
+          </div>
+      </header>
+
+
+      <section>
+        <div id='conteudo'> {/* Inicio Conteudo */}
+          <h1>BEM-VINDO</h1>
           <p>
-            Edit <code>src/App.jsx</code> and save to test <code>HMR</code>
+            A <span>IEEE IES</span> transforma desafios em soluções de tecnologia. <br></br>
+            Ao seu dispor.
           </p>
-        </div>
-        <button
-          type="button"
-          className="counter"
-          onClick={() => setCount((count) => count + 1)}
-        >
-          Count is {count}
-        </button>
+          <div id='projetos'> {/* Inicio Projetos */}
+
+            {/* inicio Card 1 */}
+            <div className='card'> 
+              <div className='bloco'>
+                        
+              </div> 
+              <div> 
+                <h4>Automoção Industrial</h4>
+                <p>Desenvolvemos sistemas que solucionam desafios de automação industrial</p>
+              </div> 
+            </div>
+            {/* Fim Card 1 */}
+
+            {/* inicio Card 2 */}
+            <div className='card'> 
+              <div className='bloco'>
+                      
+              </div> 
+              <div> 
+                <h4>Automoção Industrial</h4>
+                <p>Desenvolvemos sistemas que solucionam desafios de automação industrial</p>
+              </div> 
+            </div>
+            {/* Fim Card 2 */}
+
+            {/* inicio Card 3 */}
+            <div className='card'> 
+              <div className='bloco'>
+                     
+              </div> 
+              <div> 
+                <h4>Automoção Industrial</h4>
+                <p>Desenvolvemos sistemas que solucionam desafios de automação industrial</p>
+              </div> 
+            </div>
+            {/* Fim Card 3 */}
+
+            {/* inicio Card 4 */}
+            <div className='card'> 
+              <div className='bloco'>
+                     
+              </div> 
+              <div> 
+                <h4>Automoção Industrial</h4>
+                <p>Desenvolvemos sistemas que solucionam desafios de automação industrial</p>
+              </div> 
+            </div>
+            {/* Fim Card 4 */}
+
+         </div>{/* Fim projetos */}
+
+        </div>{/* Fim Conteudo */}
+        
+
       </section>
 
-      <div className="ticks"></div>
-
-      <section id="next-steps">
-        <div id="docs">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#documentation-icon"></use>
-          </svg>
-          <h2>Documentation</h2>
-          <p>Your questions, answered</p>
-          <ul>
-            <li>
-              <a href="https://vite.dev/" target="_blank">
-                <img className="logo" src={viteLogo} alt="" />
-                Explore Vite
-              </a>
-            </li>
-            <li>
-              <a href="https://react.dev/" target="_blank">
-                <img className="button-icon" src={reactLogo} alt="" />
-                Learn more
-              </a>
-            </li>
-          </ul>
-        </div>
-        <div id="social">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#social-icon"></use>
-          </svg>
-          <h2>Connect with us</h2>
-          <p>Join the Vite community</p>
-          <ul>
-            <li>
-              <a href="https://github.com/vitejs/vite" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#github-icon"></use>
-                </svg>
-                GitHub
-              </a>
-            </li>
-            <li>
-              <a href="https://chat.vite.dev/" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#discord-icon"></use>
-                </svg>
-                Discord
-              </a>
-            </li>
-            <li>
-              <a href="https://x.com/vite_js" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#x-icon"></use>
-                </svg>
-                X.com
-              </a>
-            </li>
-            <li>
-              <a href="https://bsky.app/profile/vite.dev" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#bluesky-icon"></use>
-                </svg>
-                Bluesky
-              </a>
-            </li>
-          </ul>
-        </div>
-      </section>
-
-      <div className="ticks"></div>
-      <section id="spacer"></section>
-    </>
+      <footer className='rodape'>
+        <p>CEFET - RJ | IEEE IES | 2026</p>
+      </footer>
+  </>
   )
 }
 
